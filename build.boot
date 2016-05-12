47ec6b118cbd16ba71d0a9c1c2a5ca32d91f8a34
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-05-12 22:09:24 +0200

    db backup

--- build.boot
+++ build.boot
@@ -1,38 +1,19 @@
 (set-env!
- :resource-paths #{"src"}
+ :source-paths #{"src"}
+ :resource-paths  #{"resources"}
  :dependencies '[[adzerk/boot-cljs            "1.7.228-1"      :scope "test"]
-                 [adzerk/boot-cljs-repl       "0.3.0"          :scope "test"]
-                 [adzerk/boot-reload          "0.4.5"          :scope "test"]
-                 [crisptrutski/boot-cljs-test "0.2.2-SNAPSHOT" :scope "test"]
                  [org.clojure/core.async "0.2.374"]
-                 [org.clojure/clojure         "1.7.0"]
-                 [org.clojure/clojurescript   "1.7.228"]
-                 [com.cemerick/piggieback     "0.2.1"          :scope "test"]
-                 [weasel                      "0.7.0"          :scope "test"]
-                 [org.clojure/tools.nrepl     "0.2.12"         :scope "test"]])
+                 [org.clojure/clojurescript   "1.7.228"]])
 
 (require
-  '[adzerk.boot-cljs      :refer [cljs]]
-  '[adzerk.boot-cljs-repl :refer [cljs-repl start-repl]]
-  '[adzerk.boot-reload    :refer [reload]]
-  '[crisptrutski.boot-cljs-test  :refer [test-cljs]])
-
-(deftask auto-test []
-  (merge-env! :resource-paths #{"test"})
-  (comp (watch)
-        (speak)
-        (test-cljs)))
-
+ '[adzerk.boot-cljs      :refer :all])
 
 (deftask build []
-  (task-options! target {:dir #{"target"}}
-                 cljs   {:optimizations :none
-                         :compiler-options {:target :nodejs}
-                         :source-map true})
+  (task-options! cljs   {:compiler-options {:optimizations :simple
+                                            :target :nodejs}})
   (comp (cljs)
         (target)))
 
 (deftask dev []
   (comp (watch)
         (build)))
-
--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,23 +1,42 @@
 (ns app.core
   (:require [cljs.nodejs :as nodejs]
+            [clojure.walk :as walk]
             [cljs.core.async :refer [<! chan >!]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+(defn ->js [event]
+  (-> event
+      js->clj
+      walk/keywordize-keys))
 
 (nodejs/enable-util-print!)
 (def AWS (nodejs/require "aws-sdk"))
 (def dd (nodejs/require "dynamodb-data-types"))
+(def doc (nodejs/require "dynamodb-doc"))
 (def attr (.-AttributeValue dd))
 (def s3 (new AWS.S3))
+(def dynamo (.DynamoDB doc))
+
+(defn handle-db-response [response]
+  (-> response
+      ->js
+      :Items))
+
+(defn get-courses []
+  (let [c (chan)
+        query {:TableName "courses"}]
+    (.scan dynamo (clj->js query) #(go (>! c (handle-db-response %2))))
+    c))
 
 (defn ->s3 [data]
   (let [c (chan)
         query {:Bucket "dynamo-events"
-               :Key "course-event.json"
+               :Key "courses.json"
                :Body (.stringify js/JSON (clj->js data))}]
     (.putObject s3 (clj->js query) #(go (>! c (clj->js %2))))
     c))
 
-(defn records->courses [records]
+#_(defn records->courses [records]
   (mapv (fn [record]
           (-> record
               (get-in ["dynamodb" "NewImage"])
@@ -25,13 +44,14 @@
               ((.-unwrap attr))
               (js->clj :keywordize-keys true))) records))
 
-(defn event->records [event]
+#_(defn event->records [event]
   (get-in event ["Records"] event))
 
-(defn handler [event context cb]
-  (let [courses (some-> (js->clj event)
-                        (event->records)
-                        (records->courses))]
-    (go
-      (cb nil (<! (->s3 checkpoints)))
+(defn ^:export handler [event context cb]
+  (go
+    (let [courses (<! (get-courses))]
+      (cb nil (<! (->s3 courses)))
       (cb "couldn't save event"))))
+
+(defn -main [] identity)
+(set! *main-cli-fn* -main)
